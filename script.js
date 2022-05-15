6865440e7a82ca92a122969f33350a72d8ff9704
Ngawang Luding <ngluding@gmail.com>
2022-05-15 00:55:11 -0400

    Added more functions to the site and did some basic styling

--- script.js
+++ script.js
@@ -1,32 +1,27 @@
 let currentState = false;
+let colour = "black";
 
 
 function getBoard(boardSize=20*20, className = "grid-20") {
     let board = document.querySelector(".board");
     let emptyDiv = board.querySelectorAll("div");
+
     emptyDiv.forEach((div) => div.remove());
-    // board.style.gridTemplateColumns = `repeat(${boardSize}, 1fr)`;
-    // board.style.gridTemplateRows = `repeat(${boardSize}, 1fr)`;
 
     for (i = 0; i < boardSize; i++) {
         let square = document.createElement("div");
         board.classList.remove("grid-20", "grid-25", "grid-30", "grid-35");
         board.classList.add(className);
-        // square.addEventListener("click", toggleColouring);
-        // square.addEventListener("mouseover", () => {
-        //     square.style.cssText = "background-color: black";
-        // })
-        square.style.cssText = "background-color: blue";
-        // square.style.cssText = "border: 1.5px solid black; height: 25px; width: 25px"
+        square.style.cssText = "background-color: rgba(0,0,0,0)";
         board.append(square);
-        // div.addEventListener("click")
     }
+
     board.addEventListener("click", toggleColouring);
 }
 
 
 function toggleColouring() {
-    const squares = document.querySelectorAll("div");
+    const squares = document.querySelectorAll(".board div");
 
     if (!currentState) {
         squares.forEach((square) => {
@@ -39,12 +34,44 @@ function toggleColouring() {
         });
         currentState = false;
     }
-    console.log(currentState);
 }
 
 
+function changeColour(selection) {
+    colour = selection
+}
+
 function colouring (e) {
-    e.target.style.backgroundColor = "black";
+    if (colour === "random") {
+        e.target.style.backgroundColor = `hsl(${Math.random() * 360}, 100%, 50%)`;
+    } else if (colour === "black") { 
+        e.target.style.backgroundColor = "black";
+    } else if (colour === "shade") {
+        shade(e);
+    } else if (colour === "custom") {
+        custom(e);
+    }
+}
+
+function custom(e) {
+    let customColour = document.querySelector("#custom").value;
+    e.target.style.backgroundColor = customColour;
+}
+
+function shade(e) {
+    let background = e.target.style.backgroundColor;
+    let opacity = background.split(" ")[3];
+    let opacityNumber = parseFloat(opacity);
+
+    console.log(background)
+    console.log(opacity);
+    console.log(opacityNumber)
+
+    if (opacityNumber === 0) {
+        e.target.style.backgroundColor = "rgba(0,0,0,0.1)";
+    } else if (opacityNumber >= 0.1 && opacityNumber <= 0.9) {
+        e.target.style.backgroundColor = `rgba(0,0,0,${opacityNumber + 0.2})`
+    }
 }
 
 
@@ -53,7 +80,7 @@ function changeSize() {
     pixel.forEach((button) => {
     button.addEventListener("click", () => {
         if(button.classList.contains("grid-20")) {
-           getBoard(20*20, "grid-20"); 
+            getBoard(20*20, "grid-20"); 
         } else if (button.classList.contains("grid-25")) {
             getBoard(25*25, "grid-25");
         } else if (button.classList.contains("grid-30")) {
@@ -61,8 +88,14 @@ function changeSize() {
         } else if (button.classList.contains("grid-35")) {
             getBoard(35*35, "grid-35");
         } 
-    })
+    }) 
 })
+}
+
+function resetBoard() {
+    let board = document.querySelector(".board");
+    let emptyDiv = board.querySelectorAll(".board div");
+    emptyDiv.forEach((div) => div.style.backgroundColor = "rgba(0,0,0,0)");
 }
 
 getBoard(20*20, "grid-20");
